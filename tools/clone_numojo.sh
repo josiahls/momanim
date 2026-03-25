b396c2823afc605d932e71dcda829820436c1aa7
josiah <jklaivins@gmail.com>
2026-03-25 18:39:28 +0000

    fix: Switch numojo clone to ssh

--- tools/clone_numojo.sh
+++ tools/clone_numojo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d third_party/NuMojo ]; then 
-    git clone https://github.com/josiahls/NuMojo.git third_party/NuMojo --branch pre-0.9 --single-branch
+    git clone git@github.com:josiahls/NuMojo.git third_party/NuMojo --branch pre-0.9
 else 
     echo 'NuMojo directory already exists.'
 fi
