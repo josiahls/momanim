b3a4f6fbf8cf77e277d7acda4b9807c0d903bbb5
josiah <jklaivins@gmail.com>
2026-03-26 17:42:29 +0000

    fix: Use separate NuMojo branch for Mojo 26.2.0.

--- tools/clone_numojo.sh
+++ tools/clone_numojo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d third_party/NuMojo ]; then 
-    git clone git@github.com:josiahls/NuMojo.git third_party/NuMojo --branch pre-0.9
+    git clone git@github.com:josiahls/NuMojo.git third_party/NuMojo --branch feature/update-mojo-26-2-0
 else 
     echo 'NuMojo directory already exists.'
 fi
