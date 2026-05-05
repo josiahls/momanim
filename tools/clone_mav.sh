5b3e801fb2766ab2440f9389c9a66cab03a5ee4b
josiah <jklaivins@gmail.com>
2026-05-05 18:31:18 +0000

    feat: Sylink local mav directory to third_party/mav if exists.

--- tools/clone_mav.sh
+++ tools/clone_mav.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+if [ ! -d third_party ]; then
+    mkdir -p third_party
+fi
+
+
 if [ ! -d third_party/mav ]; then 
-    git clone git@github.com:josiahls/mav.git third_party/mav
+    if [ -d "$HOME/mav" ]; then
+        ln -s "$HOME/mav" third_party/mav
+    else
+        git clone git@github.com:josiahls/mav.git third_party/mav
+    fi
 else 
     echo 'MAV directory already exists.'
 fi
