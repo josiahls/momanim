6804da14de684e1335879deeea77dcb4384f685c
josiah <jklaivins@gmail.com>
2026-05-22 19:18:35 +0000

    feat: Add cairo and pixman build from source tooling

--- tools/clone_cairo.sh
+++ tools/clone_cairo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -euo pipefail
+
 if [ ! -d third_party ]; then
     mkdir -p third_party
 fi
 
-
-if [ ! -d third_party/cairo ]; then 
-    git clone https://github.com/msteinert/cairo.git third_party/cairo
-else 
+if [ ! -f third_party/cairo/meson.build ]; then
+    rm -rf third_party/cairo
+    git clone --depth 1 https://gitlab.freedesktop.org/cairo/cairo.git third_party/cairo
+else
     echo 'Cairo directory already exists.'
 fi
